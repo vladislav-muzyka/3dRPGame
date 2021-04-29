3a12f82695e3feed11b7a9e30b5e8a3a3b60757f
vladislav-muzyka <mvp111hardbass@gmail.com>
2021-04-29 14:51:49 +0300

    Revert "Revert "commit""
    This reverts commit f337e31e98907e328df2d78c0b0a12bdd5666590.

--- Assets/Scripts/Inventory/Item.cs
+++ Assets/Scripts/Inventory/Item.cs
@@ -4,7 +4,16 @@ using UnityEngine;
 [CreateAssetMenu(fileName ="New Item", menuName ="Inventory/Item")]
 public class Item : ScriptableObject
 {
+    public int ID = 1;
     new public string name = "New name";
     public Sprite icon = null;
     public bool isDefaultItem = false;
+
+
+
+    public virtual void Use()
+    {
+        Debug.Log("Using "+ name);
+
+    }
 }
